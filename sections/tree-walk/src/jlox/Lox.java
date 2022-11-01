1f728eb98a95b235d8828750cfba9ac297e341df
Rossmiller, Robert <robert.rossmiller@optum.com>
2022-11-01 10:22:53 -0400

    ch 9 done

--- sections/tree-walk/src/jlox/Lox.java
+++ sections/tree-walk/src/jlox/Lox.java
@@ -16,9 +16,7 @@ public class Lox {
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
-      System.exit(64); // according
-                       // to
-                       // https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
+      System.exit(64); // https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
     } else if (args.length == 1) {
       runFile(args[0]);
     } else {
