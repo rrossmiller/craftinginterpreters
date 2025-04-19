839e1db7b653bc61822d1351dcca00a171783320
Rossmiller <robert_rossmiller+uhg@optum.com>
2025-04-19 17:57:20 -0400

    starting scanner impl

--- sections/c-vm/compiler.c
+++ sections/c-vm/compiler.c
@@ -7,4 +7,16 @@
 
 void compile(const char* source) {
     initScanner(source);
+    int line = -1;
+    while (true) {
+        Token token = scanToken();
+        if (token.line != line) {
+            line = token.line;
+        } else {
+            printf("  |");
+        }
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+        if (token.type == TOKEN_EOF)
+            break;
+    }
 }
--- sections/c-vm/scanner.c
+++ sections/c-vm/scanner.c
@@ -12,3 +12,13 @@ void initScanner(const char* source) {
     scanner.current = source;
     scanner.line = 1;
 }
+
+Token scanToken() {
+    scanner.start = scanner.current;
+
+    if (isAtEnd()) {
+        return makeToken(TOKEN_EOF);
+    }
+
+    return errorToken("Unexpected character.");
+}
